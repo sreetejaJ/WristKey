95c0748038cbe4174e72be9764f7fd37e9d373e8
Sreeteja Jonnada <sreetejajonnada@live.com>
2018-08-15 08:44:20 +0100

    Updates to main sketch

--- MainSketch/MainSketch.ino
+++ MainSketch/MainSketch.ino
@@ -2,6 +2,11 @@
 #include <U8g2lib.h>
 #include <Wire.h>
 #include <RtcDS3231.h>
+#include "EEPROM.h"
+#include "mbedtls/aes.h"
+#include "mbedtls/md.h"
+
+#define SERIAL 1    // For use when testing, set 0 for final upload
 
 //***************************RTC_START***************************
 RtcDS3231<TwoWire> Rtc(Wire);
@@ -17,9 +22,27 @@ U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 15, /* data=*/ 4, 
 
 
 
+//***************************MEM_START***************************
+#define MASTER_PASS_HASH_ADDR 0x00
+#define TAP_CODE_HASH_ADDR 0x20
+#define PACKETS_START_ADDR 0x40
+#define EEPROM_SIZE 4096
 
+//***************************MEM_END*****************************
 void setup() {
-  // put your setup code here, to run once:
+  #ifdef SERIAL
+    Serial.begin(115200);
+  #endif
+  
+//***************************MEM_START***************************
+  if (!EEPROM.begin(EEPROM_SIZE))
+  {
+    #ifdef SERIAL
+      Serial.println("failed to initialise EEPROM");
+    #endif
+  }
+  
+//***************************MEM_END*****************************
 
 //***************************OLED_SART***************************
   u8g2.begin();
